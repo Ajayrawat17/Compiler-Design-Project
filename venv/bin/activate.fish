22ce88bde82585d150451f12f805d4376d8d2b82
Ajayrawat17 <ajayrawat11146@gmail.com>
2025-05-05 10:27:36 +0530

    changes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ajay/compiler_project/venv
+set -gx VIRTUAL_ENV /home/ajay/Compiler-Design-Project/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
